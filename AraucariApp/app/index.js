73d9c2fb39f9f61731a56330ccea27602fd28ca9
Christian Verdugo <crischandel@gmail.com>
2024-08-29 19:03:20 -0400

    feat: add css configuration for web

--- AraucariApp/app/index.js
+++ AraucariApp/app/index.js
@@ -1,10 +1,11 @@
 import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import "../constants/styles.css"
 
 export default function HomeScreen() {
   return (
     <SafeAreaView className="flex items-center">
-      <Text className="text-red-600">Content is in safe area.</Text>
+      <Text className="text-black">Content is in safe area.</Text>
     </SafeAreaView>
   );
 }
