95c1ef2522cc9e447457a3923bb59638f32d591f
Linich14 <jorge_soto_1999@hotmail.com>
2024-09-09 07:09:06 -0300

    Subdivision de carpetas para componentes y vistas,adicion de imagenes en contexto y creacion del componente navbar con sus respectiva apariencia segun figma para su posterior aplicacion en las demas paginas

--- AraucariApp/web_app/src/App.jsx
+++ AraucariApp/web_app/src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
-import './App.css'
-
+import NavBar from './components/NavBar'
+import Dashboard from './components/Dashboard' 
 function App() {
   
 
   return (
-    <>
-        <h1>Hola</h1>
-    </>
+    
+    <div className="grid grid-cols-2 grid-rows-1 gap-4">
+        <div className="fixed top-0 left-0 h-screen w-64">
+          <NavBar />
+        </div>
+        <div >
+          <Dashboard />
+        </div>
+    </div>
+        
+        
+    
   )
 }
 
