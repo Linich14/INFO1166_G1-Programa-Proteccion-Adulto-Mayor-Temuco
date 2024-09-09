e9d7388b2821ff0dbb3da4154fab9c8dbdfcf24f
Lalo <lalocorreoide@outlook.com>
2024-09-09 03:07:16 -0300

    feat: Se crea el componente que reunirá los datos de confirmación de citas del prestador de servicios
    Corresponde a la parte web del Dashbaord - Prestador de servicios
    
    - Advertencia: Este componente aún no es responsivo de forma interna pero con un mínimo de ancho debería verse bien. Me queda pendiente hacer eso.

---
+++ AraucariApp/web_app/src/ModalCitas.jsx
@@ -0,0 +1,43 @@
+function ModalCitas({ Nombre, Edad, Horario }) {
+    return (
+        <article className="max-w-md mx-auto bg-white rounded-lg shadow-md border border-gray-200">
+            <section className="px-6 py-4 border-b border-gray-200 flex items-center">
+                <div className="mr-1">
+                    <img
+                        src=""
+                    />
+                </div>
+               <div className="flex-1 w-1/2">
+                    <h2 className="text-lg font-semibold">Cita en curso</h2>
+               </div>
+                <span className="flex-1 w-1/4 text-sm text-right font-medium">Asesoría Jurídica</span>
+                <hr />
+            </section>
+
+            <section className="px-6 py-4 space-y-3">
+                <div className="flex border-b border-gray-200 pb-2">
+                    <h3 className="inline-block w-1/6 border-r-2 pr-2 text-sm font-bold">Nombre</h3>
+                    <p className="text-sm ml-2">{Nombre}</p>
+                </div>
+
+                <div className="flex border-b border-gray-200 pb-2">
+                    <h3 className="inline-block w-1/6 border-r-2 pr-2 text-sm font-bold">Edad</h3>
+                    <p className="text-sm ml-2">{Edad}</p>
+                </div>
+
+                <div className="flex border-b border-gray-200 pb-2">
+                    <h3 className="inline-block w-1/6 border-r-2 pr-2 text-sm font-bold">Horario</h3>
+                    <p className="text-sm ml-2">{Horario}</p>
+                </div>
+            </section>
+
+            <section className="px-6 py-4 text-right">
+                <button className="bg-blue-600 text-sm font-medium py-2 px-4 rounded-lg text-white hover:bg-blue-700 transition-colors">
+                    Más información
+                </button>
+            </section>
+        </article>  
+    );
+}
+
+export default ModalCitas;
