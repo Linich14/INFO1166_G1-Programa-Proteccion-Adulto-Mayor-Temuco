f5abab705d78ed824e01ed032498f6a4b218c045
Linich14 <jorge_soto_1999@hotmail.com>
2024-09-09 08:56:47 -0300

    Frontend de vista Login con formulario de inicio de sesion y redireccion al home. (Se necesita cambiar la imagen de fondo a una correspondiente a la ciudad y el vector con diseño entre ambas columnas)

--- AraucariApp/web_app/src/views/Login.jsx
+++ AraucariApp/web_app/src/views/Login.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
-
-const Login = () => {
+import { Link } from 'react-router-dom';
+function Login() {
   return (
-    <main className="p-4">
-      <h1 className="text-3xl font-bold">Bienvenido a mi página</h1>
-      <p className="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.</p>
+    <main className="grid grid-cols-2 grid-rows-1 gap-4 h-screen Login">
+      <div className="flex justify-center items-center difuminado">
+        <div className=" p-4 rounded-lg shadow-lg w-1/2 ">
+          <h1 className="text-3xl font-bold mb-6">Inicio de Sesión</h1>
+          <form action="" className="mt-4">
+            <label className="block mb-2" htmlFor="username">Nombre de usuario:</label>
+            <input type="text" id="username" className="block w-full p-2 border-b-2 border-white focus:border-b-2 focus:border-blue-500 outline-none" />
+            <label className="block mb-2" htmlFor="password">Contraseña:</label>
+            <input type="password" id="password" className="block w-full p-2 border-b-2 border-white focus:border-b-2 focus:border-blue-500 outline-none" />
+            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg mt-4">Iniciar sesión</button>
+            <a href="/" className="text-white hover:text-blue-700 p-3">¿Olvidaste tu contraseña?</a>
+          </form>
+        </div>
+      </div>
+      <div className="flex justify-center items-center">
+        <div className="bg-white p-4 rounded-lg shadow-lg  justify-center items-center ">
+          <img src="../../Logocolor.png" alt="" />
+        </div>
+      </div>
     </main>
   );
-};
+}
 
 export default Login;
