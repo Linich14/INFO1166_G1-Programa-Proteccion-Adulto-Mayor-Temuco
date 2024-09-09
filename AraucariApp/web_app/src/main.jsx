1235beafffd496d11f545ec14e8b3e9059cdbca9
Linich14 <jorge_soto_1999@hotmail.com>
2024-09-09 09:07:39 -0300

    Correccion de un error de tipaje en solicitud de inicio de sesion y adicion de vista olvidar contraseña sin elementos(solo redirecciona y muestra un h1)

--- AraucariApp/web_app/src/main.jsx
+++ AraucariApp/web_app/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from './App.jsx'
 import Login from './views/Login.jsx'
+import ReiniciarContrasena from './views/OlvidarContraseña.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/resetpassword" element={<ReiniciarContrasena />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
--- AraucariApp/web_app/src/views/Login.jsx
+++ AraucariApp/web_app/src/views/Login.jsx
@@ -7,12 +7,13 @@ function Login() {
         <div className=" p-4 rounded-lg shadow-lg w-1/2 ">
           <h1 className="text-3xl font-bold mb-6">Inicio de Sesión</h1>
           <form action="" className="mt-4">
-            <label className="block mb-2" htmlFor="username">Nombre de usuario:</label>
-            <input type="text" id="username" className="block w-full p-2 border-b-2 border-white focus:border-b-2 focus:border-blue-500 outline-none" />
+            <label className="block mb-2" htmlFor="username" >Rut o correo de funcionario:</label>
+            <input type="text" id="username" placeholder='12.345.678-9 o funcionariosTemuco@outlook.com' className="block w-full p-2 border-b-2 border-white focus:border-b-2 focus:border-blue-500 outline-none mb-4" />
             <label className="block mb-2" htmlFor="password">Contraseña:</label>
-            <input type="password" id="password" className="block w-full p-2 border-b-2 border-white focus:border-b-2 focus:border-blue-500 outline-none" />
-            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg mt-4">Iniciar sesión</button>
-            <a href="/" className="text-white hover:text-blue-700 p-3">¿Olvidaste tu contraseña?</a>
+            <input type="password" id="password" placeholder='*****************' className="block w-full p-2 border-b-2 border-white focus:border-b-2 focus:border-blue-500 outline-none" />
+            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg mt-4" >Iniciar sesión</button>
+            <a href="/" className="text-white hover:text-blue-700 p-3">Home temporal</a>
+            <a href="/resetpassword" className="text-white hover:text-blue-700 p-3">¿Olvidaste tu contraseña?</a>
           </form>
         </div>
       </div>
