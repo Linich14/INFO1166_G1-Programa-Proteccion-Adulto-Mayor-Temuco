618e73895fdaeda4ef00dfade82265d49c97edc4
Christian Verdugo <crischandel@gmail.com>
2024-09-10 13:30:56 -0300

    feat: Sección de Recuperar contraseña

--- AraucariApp/app/(tabs)/index.js
+++ AraucariApp/app/(tabs)/index.js
@@ -4,16 +4,16 @@ import "../../constants/styles.css";
 import { Link, Redirect } from "expo-router";
 
 export default function Index() {
-  const auth = false;
+	const auth = false;
 
-  if (!auth) {
-    return <Redirect href="(auth)/login" />;
-  }
+	if (!auth) {
+		return <Redirect href="(auth)/login" />;
+	}
 
-  return (
-    <SafeAreaView className="flex items-center">
-      <Link href="/(auth)/login">Inicio de Cesion</Link>
-      <Text className="text-black">Content is in safe area.</Text>
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView className="flex items-center">
+			<Link href="/(auth)/login">Inicio de Cesion</Link>
+			<Text className="text-black">Content is in safe area.</Text>
+		</SafeAreaView>
+	);
 }
