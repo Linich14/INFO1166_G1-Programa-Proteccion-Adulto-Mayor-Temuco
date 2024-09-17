89dce6d02bd7e8fd25084cdc3ee4662e79dde8f4
Lalo <lalocorreoide@outlook.com>
2024-09-16 23:45:44 -0300

    Fix: Avance de correción del componentes `contador de estados de servicio`

--- AraucariApp/web_app/src/components/ContadorEstadosServicios.jsx
+++ AraucariApp/web_app/src/components/ContadorEstadosServicios.jsx
@@ -1,31 +1,39 @@
 
 import Pending from '../../public/Pending.svg?react';
-import CircleCheck from '../../public/CircleCheck.svg?react';
+import CircleCheck  from '../../public/CircleCheck.svg?react';
 import Cancel from '../../public/Cancel.svg?react';
 
 
 function ContadorHorasServicio() {
     return (
-        <article className="flex flex-wrap gap-4 justify-between">
-            {/* Sección de Pendientes */}
-            <section className="bg-white rounded-lg shadow-md p-4 w-full sm:w-1/4 flex flex-col items-center aspect-square">
-                <img src={Pending} alt="Pending" className="w-10 h-10 mb-2" />
-                <h2 className="font-semibold text-xl">Pendientes</h2>
-                <p className="text-3xl mt-2">23</p>
+        <article className="flex flex-wrap jgap-4">
+
+            <section className="bg-white rounded-lg shadow-md p-4 aspect-square w-full w-1/3 ">
+                <div className="flex flex-col items-center">
+                    <div className="flex flex-row items-center w-full">
+                        <img src={Pending} alt="Pending" className="basis-1/5"/>    
+                        <h2 className="font-semibold text-2xl basis-3/5">Pendientes</h2>
+                        <p className="text-2xl basis-1/5 text-right">23</p>
+                    </div>
+                </div>
             </section>
 
-            {/* Sección de Finalizados */}
-            <section className="bg-white rounded-lg shadow-md p-4 w-full sm:w-1/4 flex flex-col items-center">
-                <img src={CircleCheck} alt="Finalizados" className="w-10 h-10 mb-2" />
-                <h2 className="font-semibold text-xl">Finalizados</h2>
-                <p className="text-3xl mt-2">23</p>
+            <section className="bg-white rounded-lg shadow-md p-4 aspect-square">
+                <div className="flex flex-row items-center">
+                    <img src={CircleCheck} alt="Pending" className="basis-1/5" />
+                    <h2 className="font-semibold text-2xl basis-4/5">Finalizados</h2>
+                </div>
+                <div className="flex justify-center">
+                    <span className="text-2xl">23</span>
+                </div>
             </section>
 
-            {/* Sección de Cancelados */}
-            <section className="bg-white rounded-lg shadow-md p-4 w-full sm:w-1/4 flex flex-col items-center">
-                <img src={Cancel} alt="Cancelados" className="w-10 h-10 mb-2" />
-                <h2 className="font-semibold text-xl">Cancelados</h2>
-                <p className="text-3xl mt-2">23</p>
+            <section className="bg-white rounded-lg shadow-md p-4 aspect-square">
+                <div className="flex flex-row flex-wrap items-center">
+                    <img src={Cancel} alt="Pending" className="basis-1/5" />
+                    <h2 className="font-semibold text-2xl basis-4/5">Cancelados</h2>
+                </div>
+                    <p className="text-2xl text-center">23</p>
             </section>
         </article>
     )
