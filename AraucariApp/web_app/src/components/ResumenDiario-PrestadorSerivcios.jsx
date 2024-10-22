4bcccb0f447ba14f28d422b1fb0697084880ddf8
Lalo <lalocorreoide@outlook.com>
2024-10-22 08:57:16 -0300

    Fix: Corrección de error de consola

--- AraucariApp/web_app/src/components/ResumenDiario-PrestadorSerivcios.jsx
+++ AraucariApp/web_app/src/components/ResumenDiario-PrestadorSerivcios.jsx
@@ -1,6 +1,6 @@
-function ResumenDiarioServicios ({ atenciones }) {
+function ResumenDiarioServicios({ atenciones }) {
     return (
-        <article className=" bg-white rounded-lg shadow-md border border-gray-200 p-4">
+        <article className="bg-white rounded-lg shadow-md border border-gray-200 p-4">
             <section className="flex items-center justify-between mb-4">
                 <h2 className="text-2xl font-semibold">Resumen Diario</h2>
                 <span>Martes 28 de agosto</span>
@@ -16,14 +16,10 @@ function ResumenDiarioServicios ({ atenciones }) {
                             <th className="border-b">Estado</th>
                         </tr>
                     </thead>
-                    {/* Datos de prueba, pero aquí se debería mostrar por cada fila los datos de un usuario */}
                     
                     <tbody className="text-xl">
-                        {atenciones.map((atencion) => (
-                            <tr key={atencion.id}>
-                                <td>{atencion.hora}</td>
-                                <td>{atencion.cliente_nombre}</td> {/* Aquí muestra el nombre de la cita */}
-                            </tr>
+                        {atenciones.map(atencion => (
+                            <tr key={atencion.id}><td>{atencion.hora}</td><td>{atencion.cliente_nombre}</td></tr>
                         ))}
                     </tbody>
                 </table>
