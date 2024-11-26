3302f5f524631fd1b78516a729e05ab72a573b41
Delkira544 <crischandel@gmail.com>
2024-11-26 11:58:16 -0300

    Fix

--- BackendApp/Usuario/urls.py
+++ BackendApp/Usuario/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
   # path('inicio_sesion/', InicioSesion_Usuario.as_view()),
     path('registro/', Registro_Usuario.as_view()),
     path('usuario/', UsuarioDetalles.as_view()),
-    path('usuariosLista/', AutentificacionUsuario.as_view())
-] 
+    path('usuariosLista/', AutentificacionUsuario.as_view()),
+     
     path('actualizar_usuario/', ActualizarUsuario.as_view(), name='Actualizar_Usuario'),
     path('subir_foto_perfil/', Subir_foto_perfil.as_view(), name='subir_foto_perfil'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
